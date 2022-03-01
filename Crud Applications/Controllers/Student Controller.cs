a3b627f61855e257414a8e188fef7b043f88edbe
Chandan <Chandan@DESKTOP-7G0P5Q8>
2022-02-28 22:29:54 -0800

    Add project files.

---
+++ Crud Applications/Controllers/Student Controller.cs
@@ -0,0 +1,30 @@
+﻿using Crud_Applications.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud_Applications.Controllers
+{
+    public class Student_Controller : Controller
+    {
+        StudentDbContext db;
+        public Student_Controller(StudentDbContext context)
+        {
+            db = context; 
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult create(Student ampob)
+        {
+            db.Students.Add(ampob);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ Crud Applications/Controllers/db.cs
@@ -0,0 +1,6 @@
+﻿namespace Crud_Applications.Controllers
+{
+    internal class db
+    {
+    }
+}
